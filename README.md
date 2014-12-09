ebf94a18381268db575ffbba17b4519f276fdff8
meru_akimbo <merukatoruayu0@gmail.com>
2014-12-09 20:30:58 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Amon2::Setup::Flavor::ModelResource - It's new $module
+
+# SYNOPSIS
+
+    use Amon2::Setup::Flavor::ModelResource;
+
+# DESCRIPTION
+
+Amon2::Setup::Flavor::ModelResource is ...
+
+# LICENSE
+
+Copyright (C) meru\_akimbo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+meru\_akimbo <merukatoruayu0@gmail.com>
---
+++ lib/Amon2/Setup/Flavor/ModelResource.pm
@@ -0,0 +1,39 @@
+package Amon2::Setup::Flavor::ModelResource;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Amon2::Setup::Flavor::ModelResource - It's new $module
+
+=head1 SYNOPSIS
+
+    use Amon2::Setup::Flavor::ModelResource;
+
+=head1 DESCRIPTION
+
+Amon2::Setup::Flavor::ModelResource is ...
+
+=head1 LICENSE
+
+Copyright (C) meru_akimbo.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+meru_akimbo E<lt>merukatoruayu0@gmail.comE<gt>
+
+=cut
+
